bfee91effa8309e7068a35a4dcf97285e1444fb0
ziyucao <caoziyu1997@gmail.com>
2018-01-10 22:31:14 +0800

    屏蔽页面底部广告 屏蔽播放器下方游戏广告 屏蔽页面顶部大图 屏蔽左边导航栏

--- js/main.js
+++ js/main.js
@@ -8,71 +8,84 @@ lz.firstRun = true;
 lz.timeout;
 
 
-lz.toggle = function(status){
+lz.toggle = function (status) {
 
-    lz.allowed = status;	
+    lz.allowed = status;
 
-    if( lz.allowed ){
-	
+    if (lz.allowed) {
+
         start();
-		
-    }else{
-	
+
+    } else {
+
         pause();
-		
+
     }
 
-    function start(){
-	
-		removeAd();
-	
-        if( lz.allowed ) requestAnimationFrame( checker );
-		
-		if( lz.firstRun ) lz.firstRun = false;
-		
-    }
-	
-	function removeAd(){
-	
-		$( '.chatroom-ap' ).remove();
-	
-		$( '#chat-limit-scroll-container' ).find( '.effect-panel-outer' ).hide();
-		
-	}
-    
-    function checker(){
-	
-		$( '#live-player' ).find( '.tp-link.report-rbi-static' ).hide()
-		
-		$( '#chat-limit-scroll-container' ).find( '.effect-panel-outer' ).hide();
-		
-		$( '#effect-animate' ).hide();
-		
-		$( '#chatroom' ).find( '.effect-panel-outer' ).hide();
-	
-        var a = $( '#chat-limit-scroll-container' ).find( '.pt-chat-plugins-cell' );
+    function start() {
 
-		var n = a.length;
+        removeAd();
 
-		while(--n > 0){
+        if (lz.allowed) requestAnimationFrame(checker);
 
-			var el = $( '#chat-limit-scroll-container' ).find('.pt-chat-plugins-cell')[n];
+        if (lz.firstRun) lz.firstRun = false;
 
-			if( $(el).find('.send-text').length ) $(el).hide();
-			
-			var re = new RegExp( /^\d{1,}(6)$/ );
-			
-			if( re.test( $(el).find('.message-content').text() ) ) $(el).hide();
-			
-		}
-		
-		if( lz.allowed ) requestAnimationFrame( checker );
     }
 
-    function pause(){
-	
+    function removeAd() {
+        $('.quiz-intrduction').remove();
+        $('.images-link-sp').attr("style", "margin-bottom:160px");
+        $('.stage-inner').attr("style", "position: relative; padding-right:6rem; padding-bottom:50px");
+        $('.image-link-top').remove();
+        $('.header').parent().attr("class","stage-container-head");
+        $('.gift-radio').parent().remove();
+        $('.topbar-nav-yzcm').remove();
+        $('.topbar-nav-shop-hover').remove();
+        $('.tencent-popup').remove();
+        $('.live-footer-hotgames').remove();
+        $('.live-footer-games').remove();
+        $('.official-activities').remove();
+        $('.challenge-task').remove();
+        $('.simplesidenav-nav').remove();
+        $('.stage').attr("style", "");
+        $('.top-banner-pic').remove();
+        $('.chatroom-ap').remove();
+        $('#chat-limit-scroll-container').find('.effect-panel-outer').hide();
+    }
+
+    function checker() {
+
+        $('#live-player').find('.tp-link.report-rbi-static').hide()
+
+        $('#chat-limit-scroll-container').find('.effect-panel-outer').hide();
+
+        $('#effect-animate').hide();
+
+        $('#chatroom').find('.effect-panel-outer').hide();
+
+        var a = $('#chat-limit-scroll-container').find('.pt-chat-plugins-cell');
+
+        var n = a.length;
+
+        while (--n > 0) {
+
+            var el = $('#chat-limit-scroll-container').find('.pt-chat-plugins-cell')[n];
+
+            if ($(el).find('.send-text').length) $(el).hide();
+
+            var re = new RegExp(/^\d{1,}(6)$/);
+
+            if (re.test($(el).find('.message-content').text())) $(el).hide();
+
+        }
+
+        if (lz.allowed) requestAnimationFrame(checker);
+    }
+
+    function pause() {
+
         cancelAnimationFrame();
-		
+
     }
-	
+
 }
