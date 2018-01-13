2060ad37fc10226795a77d9880ae1217b796a551
ziyucao <caoziyu1997@gmail.com>
2018-01-14 01:18:40 +0800

    将头条替换成房间公告 屏蔽细节修正 页面布局优化 进一步圆角化

--- js/main.js
+++ js/main.js
@@ -35,41 +35,33 @@ lz.toggle = function(status){
     }
 
     function removeAd() {
-        /*
+        /**
         * @author ziyucao
         */
-        $('.gallery').remove();
         $('.quiz-intrduction').remove();
-        $('.images-link-sp').attr("style", "margin-bottom:160px");
-        $('.stage-inner').attr("style", "position: relative; padding-right:6rem; padding-bottom:50px");
-        $('.image-link-top').remove();
-        $('.header').parent().attr("class","stage-container-head");
-        $('.gift-radio').parent().remove();
+        // 底部广告屏蔽
+        $('.stage-container-widget').remove();
         $('.topbar-nav-yzcm').remove();
         $('.topbar-nav-shop-hover').remove();
         $('.tencent-popup').remove();
         $('.live-footer-hotgames').remove();
         $('.live-footer-games').remove();
         $('.official-activities').remove();
-        $('.challenge-task').remove();
-        $('.simplesidenav-nav').remove();
-        $('.stage').attr("style", "");
         $('.top-banner-pic').remove();
         $('.chatroom-ap').remove();
+        $('.lobster-small').remove();
 
-		$('#right-nav').remove();
-        $('#chat-limit-scroll-container').find('.effect-panel-outer').hide();
-		
+        $( '#right-nav' ).remove();
+
+        $( '#chat-limit-scroll-container' ).find( '.effect-panel-outer' ).hide();
+
         $( '.chatroom-con' ).css( 'user-select','auto' );
 		$( '#right-nav' ).remove();
-		
-		$( '.gallery' ).remove();
-		$( '.bottom-image-small' ).remove();
     }
 	
 	function optimizeCss(){
 	
-		$( '.header' ).css( 'border-radius' , '4px 4px 4px 4px' );
+		$( '.header' ).css( 'border-radius' , '4px 0px 0px 4px' );
 		
 		$( '.stage-container-right' ).css( 'border-radius','0px 4px 4px 0px' );
 		
@@ -82,9 +74,51 @@ lz.toggle = function(status){
 		$( '#gift-history-list-scroll' ).hide();
 		
 		$( '.gift-history-list' ).append( $('#guess') );
-		
-		
-		
+
+
+        /**
+        * @author ziyucao
+        */
+        // 移除通关进度
+        $('.challenge-task').remove();
+
+        // 移除左边导航栏
+        $('.simplesidenav-nav').remove();
+        $('.stage').css('padding-left', '0');
+
+        // 页面padding调整
+        $('.stage-inner').css({'padding-right': '14px',
+            'padding-bottom': '50px'});
+        $('.ptc-footer').css({'margin-top': '0',
+            'border-top': '0'});
+
+        // 将头条替换成房间公告
+        $('.announcement').insertBefore($('.gift-radio'));
+        $('.announcement').css({'border': 'none',
+            'border-radius': '0px 4px 4px 0px',
+            'height': '120px'});
+        $('.announcement-container').css('height', 'auto');
+        $('#announcement-content').css('height', 'auto');
+        $('.announcement-title').css('padding-left', '40px');
+        $('.announcement-title i').css('left', '15px');
+        $('.gift-radio').hide();
+
+        // 圆角化
+        $('.play_back').css('border-radius', '4px 4px 4px 4px');
+        $('.stage-container-left').css('border-radius', '4px 4px 4px 4px');
+        $('#live-footer-tabs').css('border-radius', '0 0 0 4px');
+        $('.live-footer-tabs-inner').css('border-radius', '0 0 0 4px');
+        $('#chatroom').css('border-radius', '0 4px 4px 0');
+        $('.showFans').css('border-radius', '4px 0 0 4px');
+        $('#chatroom-button').css('border-radius', '0 4px 4px 0');
+        $('.feed-pane-count').css({'border-radius': '4px 4px 4px 4px',
+            'padding': '0 6px 0 4px'});
+        $('.feed-pane').css({'border-radius': '4px 4px 4px 4px',
+            'padding-right': '8px'});
+        $('.header-avatar-img').css('border-radius', '4px');
+
+        // 输入区域颜色调整
+        // $('.chatroom-editor').css('background-color', '#fff');
 	}
 
     function checker(){
@@ -120,6 +154,12 @@ lz.toggle = function(status){
             if( re.test( $(el).find('.message-content').text() ) ) $(el).hide();
             
         }
+
+        /**
+         * @author ziyucao
+         */
+        // 移除左边导航栏后页面调整
+        $('.stage').css('padding-left', '0');
         
         if( lz.allowed ) requestAnimationFrame( checker );
     }
